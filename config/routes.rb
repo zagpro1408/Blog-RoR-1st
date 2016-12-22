6781780dbfaf751764f442889773e652f0aa94b0
zagpro1408 <zagorskybobuyc@gmail.com>
2016-12-22 12:24:15 +0300

    Init app

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  resource :contacts, only: [:new, :create]
+  resources :articles
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
