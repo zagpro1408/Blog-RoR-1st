0a0ae0da7419eec80643e871b8581d41bd89762c
zagpro1408 <zagorskybobuyc@gmail.com>
2016-12-22 13:39:56 +0300

    Change /contacts/new -> /contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,13 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.new contact_params
-    @contact.save
+
+    # проверка email и message на пустые поля
+    if @contact.valid?
+      @contact.save
+    else
+      render action: 'new'
+    end
   end
 
   private
